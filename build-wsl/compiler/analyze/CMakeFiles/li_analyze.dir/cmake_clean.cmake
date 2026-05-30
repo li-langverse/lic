1d009d0e7df88c570436d377d636a38c9c9844bb
Julian Kleber <julian.kleber@sail.black>
2026-05-30 15:06:53 +0200

    merge main into chore/agent-docs_maintainer-1780144262784 (prefer main)

---
+++ build-wsl/compiler/analyze/CMakeFiles/li_analyze.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/li_analyze.dir/advisory.cpp.o"
+  "CMakeFiles/li_analyze.dir/advisory.cpp.o.d"
+  "libli_analyze.a"
+  "libli_analyze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/li_analyze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

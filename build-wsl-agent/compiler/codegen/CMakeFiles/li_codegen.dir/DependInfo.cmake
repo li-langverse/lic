216f050db49cee15e54051da94a9322d1b3cb154
Julian Kleber <julian.kleber@sail.black>
2026-05-31 05:19:54 +0200

    chore(lic): world_studio_builder post-hook commit
    Li-Agent-Run: world_studio_builder-1780197033142
    Li-Agent-Id: world_studio_builder

---
+++ build-wsl-agent/compiler/codegen/CMakeFiles/li_codegen.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Julian/Documents/Programming/li/li-cursor-agents/data/workspaces/li-langverse/lic/world_studio_builder-1780197033142/repo/compiler/codegen/codegen.cpp" "compiler/codegen/CMakeFiles/li_codegen.dir/codegen.cpp.o" "gcc" "compiler/codegen/CMakeFiles/li_codegen.dir/codegen.cpp.o.d"
+  "/mnt/c/Users/Julian/Documents/Programming/li/li-cursor-agents/data/workspaces/li-langverse/lic/world_studio_builder-1780197033142/repo/compiler/codegen/compile.cpp" "compiler/codegen/CMakeFiles/li_codegen.dir/compile.cpp.o" "gcc" "compiler/codegen/CMakeFiles/li_codegen.dir/compile.cpp.o.d"
+  "/mnt/c/Users/Julian/Documents/Programming/li/li-cursor-agents/data/workspaces/li-langverse/lic/world_studio_builder-1780197033142/repo/compiler/codegen/emit.cpp" "compiler/codegen/CMakeFiles/li_codegen.dir/emit.cpp.o" "gcc" "compiler/codegen/CMakeFiles/li_codegen.dir/emit.cpp.o.d"
+  "/mnt/c/Users/Julian/Documents/Programming/li/li-cursor-agents/data/workspaces/li-langverse/lic/world_studio_builder-1780197033142/repo/compiler/codegen/smoke_llvm.cpp" "compiler/codegen/CMakeFiles/li_codegen.dir/smoke_llvm.cpp.o" "gcc" "compiler/codegen/CMakeFiles/li_codegen.dir/smoke_llvm.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
